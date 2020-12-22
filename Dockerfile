c9b7a4efbe9791b7c456636877d514b62b951f4b
limjinyung <jinyung0069@hotmail.com>
2020-12-22 10:25:20 +0800

    moedifying files to test Selenium in Jenkins

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,3 @@ WORKDIR /helloworld
 COPY requirements.txt /helloworld/
 RUN pip install -r requirements.txt
 COPY . /helloworld/
-EXPOSE 8000
-CMD ["gunicorn"  , "--bind", "0.0.0.0:8000", "helloworld.wsgi"]
--- sms/tests.py
+++ sms/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Create your tests here.
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://localhost:8000/view/#")
+
+# get the search textbox
+search_field = driver.find_elements_by_tag_name('td')
+
+passed=1
+
+for listitem in search_field:
+   if listitem.get_attribute("innerHTML") != "Lim Jin Yung":
+      passed = 0
+   if listitem.get_attribute("innerHTML") != "Khoo Loo Li":
+      passed = 0
+
+if not passed:
+   driver.quit()
+   raise AssertionError()
+
+# close the browser window
+driver.quit()
